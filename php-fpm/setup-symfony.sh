be5d23194c624ed1ced665fd9cc59b25a1f06ca3
Etienne <etienne.daumail@smartfizz.fr>
2021-05-27 14:48:26 +0200

    update

--- php-fpm/setup-symfony.sh
+++ php-fpm/setup-symfony.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-[ -e /var/www/symfony/var ] || ln -s /var/www/app/var /var/www/symfony/var
+[ -e /var/www/symfony/var/logs ] || ln -s /var/www/app/var/logs /var/www/symfony/var/logs
+[ -e /var/www/symfony/var/cache ] || ln -s /var/www/app/var/cache /var/www/symfony/var/cache
 [ -e /var/www/symfony/vendor ] || ln -s /var/www/app/vendor /var/www/symfony/vendor
